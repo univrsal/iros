707fb0e19d633ebe370c7893640ff15674efaff0
univrsal <uni@vrsal.xyz>
2025-01-16 22:19:06 +0100

    Iframe: Add warning for youtube videos.

--- static/js/elements/iframe.js
+++ static/js/elements/iframe.js
@@ -1,6 +1,6 @@
 /*
    This file is part of iros
-   Copyright (C) 2024 Alex <uni@vrsal.xyz>
+   Copyright (C) 2025 Alex <uni@vrsal.xyz>
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU Affero General Public License as published
@@ -15,108 +15,128 @@
    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
+var yt_warning_show_count = get_cookie('yt_warning_show_count') || 0;
 class iframe_element extends element {
-    constructor(parent, data) {
-        super(parent, "iframe", data);
-        this.html = $(`<div class="iros-element"  id="${this.data.id}"></div>`);
-        this.blocker = $(`<div class="iros-blocker"></div>`);
-        this.iframe = $(`<iframe class="iros-iframe" src="${this.data.url}" frameborder="0"></iframe>`);
-        this.html.append(this.iframe);
-        this.html.append(this.blocker);
-        this.block_events = data.block_events;
+  constructor(parent, data) {
+    super(parent, 'iframe', data);
+    this.html = $(`<div class="iros-element"  id="${this.data.id}"></div>`);
+    this.blocker = $(`<div class="iros-blocker"></div>`);
+    this.iframe = $(
+      `<iframe class="iros-iframe" src="${this.data.url}" frameborder="0"></iframe>`
+    );
+    this.html.append(this.iframe);
+    this.html.append(this.blocker);
+    this.block_events = data.block_events;
 
-        if (parent.is_editor()) {
-            $(this.html).on("load", (event) => {
-                console.log(event);
-            });
-        }
+    if (parent.is_editor()) {
+      $(this.html).on('load', (event) => {
+        console.log(event);
+      });
+    }
+  }
+
+  set_url(url) {
+    if (this.iframe.src == url) return false;
+
+    // validate url by turning it into a URL object
+    try {
+      let _tmp = new URL(url);
+    } catch (e) {
+      console.log('Invalid URL: ', url);
+      return false;
     }
 
-    set_url(url) {
-        if (this.iframe.src == url)
-            return false;
-
-        // validate url by turning it into a URL object
-        try {
-            let _tmp = new URL(url);
-        } catch (e) {
-            console.log("Invalid URL: ", url);
-            return false;
-        }
-
-        this.iframe.src = url;
-        this.data.url = url;
-        return true;
+    // check if it's a youtube video
+    if (url.indexOf('youtube.com') !== -1 || url.indexOf('youtu.be') !== -1) {
+      // notify the user that youtube videos are not synced
+      if (yt_warning_show_count < 2) {
+        alert(
+          `YouTube videos are not synced across clients. The video will only play for you. If you need synchronized video playback consider uploading the video somewhere as a file and embed it using the video element with a direct link to the video file (i.e. ending in .mp4/.webm etc.). This warning will be shown only twice.`
+        );
+        yt_warning_show_count++;
+        set_cookie('yt_warning_show_count', yt_warning_show_count, 365);
+      }
     }
 
-    set_block_events(block_events) {
-        this.data.block_events = block_events;
-        this.blocker.style.display = this.data.block_events ? "block" : "none";
-        return true;
-    }
+    this.iframe.src = url;
+    this.data.url = url;
+    return true;
+  }
 
-    update() {
-        super.update();
-        this.set_url(this.data.url);
-        this.set_block_events(this.data.block_events);
-        this.html.style.width = `${this.data.transform.width}px`;
-        this.html.style.height = `${this.data.transform.height}px`;
-    }
+  set_block_events(block_events) {
+    this.data.block_events = block_events;
+    this.blocker.style.display = this.data.block_events ? 'block' : 'none';
+    return true;
+  }
 
-    is_resizable() { return true; }
+  update() {
+    super.update();
+    this.set_url(this.data.url);
+    this.set_block_events(this.data.block_events);
+    this.html.style.width = `${this.data.transform.width}px`;
+    this.html.style.height = `${this.data.transform.height}px`;
+  }
+
+  is_resizable() {
+    return true;
+  }
 }
 
 class iframe_element_handler extends element_handler {
-    constructor(edt) {
-        super(edt, "iframe");
-        this.url = $("#url-input");
-        this.block_events = $("#block-events");
-        this.url_settings = $("#url-settings");
-        this.event_settings = $("#event-settings");
-        this.block_events.on("change", () => this.update_selected_element());
-        this.url.on("input", () => this.update_selected_element());
-    }
+  constructor(edt) {
+    super(edt, 'iframe');
+    this.url = $('#url-input');
+    this.block_events = $('#block-events');
+    this.url_settings = $('#url-settings');
+    this.event_settings = $('#event-settings');
+    this.block_events.on('change', () => this.update_selected_element());
+    this.url.on('input', () => this.update_selected_element());
+  }
 
-    update_selected_element() {
-        if (this.selected_element) {
-            let changed = false;
-            changed |= this.selected_element.set_url(this.url.value);
-            changed |= this.selected_element.set_block_events(this.block_events.checked);
-            if (changed)
-                send_command_update_element(this.edt, this.selected_element);
-        }
+  update_selected_element() {
+    if (this.selected_element) {
+      let changed = false;
+      changed |= this.selected_element.set_url(this.url.value);
+      changed |= this.selected_element.set_block_events(
+        this.block_events.checked
+      );
+      if (changed) send_command_update_element(this.edt, this.selected_element);
     }
+  }
 
-    show_settings(element) {
-        this.url_settings.style.display = "grid";
-        this.event_settings.style.display = "grid";
-        this.url.value = element.data.url;
-        this.selected_element = element;
-        this.block_events.checked = element.data.block_events;
-    }
+  show_settings(element) {
+    this.url_settings.style.display = 'grid';
+    this.event_settings.style.display = 'grid';
+    this.url.value = element.data.url;
+    this.selected_element = element;
+    this.block_events.checked = element.data.block_events;
+  }
 
-    hide_settings() {
-        this.url_settings.style.display = "none";
-        this.event_settings.style.display = "none";
-        this.selected_element = null;
-    }
+  hide_settings() {
+    this.url_settings.style.display = 'none';
+    this.event_settings.style.display = 'none';
+    this.selected_element = null;
+  }
 }
 
-function add_iframe_element(url = null, name = null, width = 480, height = 360) {
-    if (url === null)
-        url = "https://example.com";
-    if (name === null)
-        name = `Iframe ${edt.get_next_element_id()}`;
-    let data = {
-        url,
-        block_events: true,
-        transform: {
-            x: 0,
-            y: 0,
-            width,
-            height,
-        },
-        name
-    };
-    edt.add_element(create_element(edt, "iframe", data));
+function add_iframe_element(
+  url = null,
+  name = null,
+  width = 480,
+  height = 360
+) {
+  if (url === null) url = 'https://example.com';
+  if (name === null) name = `Iframe ${edt.get_next_element_id()}`;
+  let data = {
+    url,
+    block_events: true,
+    transform: {
+      x: 0,
+      y: 0,
+      width,
+      height,
+    },
+    name,
+  };
+  edt.add_element(create_element(edt, 'iframe', data));
 }
