d4053139368d1d9b9d789fd219fd01e8fb0a0f5c
univrsal <uni@vrsal.xyz>
2025-01-16 22:13:43 +0100

    Misc: New discord cdn link

--- static/js/editor/key_binds.js
+++ static/js/editor/key_binds.js
@@ -16,197 +16,251 @@
 */
 
 function keybind_copy(edit, _e) {
-    if (edit.selected_element)
-        edit.internal_clipboard = edit.selected_element;
+  if (edit.selected_element) edit.internal_clipboard = edit.selected_element;
 }
 
-var warning_show_count = get_cookie("warning_show_count") || 0;
+var warning_show_count = get_cookie('warning_show_count') || 0;
 
 function check_dc(data) {
-    if (data.indexOf("cdn.discordapp.com") !== -1 && warning_show_count < 2) {
-        alert(`Discord CDN links are temporary. Any linked media will fail to load after the link expires. Consider uploading the media to a more permanent host. This warning will be only twice.`);
-        warning_show_count++;
-        set_cookie("warning_show_count", warning_show_count, 365);
-    }
+  if (
+    (data.indexOf('cdn.discordapp.com') !== -1 ||
+      data.indexOf('media.discordapp.net') !== -1) &&
+    warning_show_count < 2
+  ) {
+    alert(
+      `Discord image links are temporary. Any linked media will fail to load after the link expires. Consider uploading the media to a more permanent host. This warning will be only twice.`
+    );
+    warning_show_count++;
+    set_cookie('warning_show_count', warning_show_count, 365);
+  }
 }
 
 function keybind_paste(edit, _e) {
-    read_data_from_clipboard().then(data => {
-        let id = generate_element_id();
-        if (data.indexOf("data:image") === 0) {
-            let name = edit.make_unique_element_name("Image");
-            edit.add_element(new image_element(edit, { url: data, id, name }));
-        } else if (/(https?:\/\/.*\.(?:jpg|jpeg|gif|png|bmp|webp|svg|ico|avif|jxl))(?:\?.*)?$/i.test(data)) { // check if it's an image url
-            check_dc(data);
-            let name = edit.make_unique_element_name("Image");
-            edit.add_element(new image_element(edit, { url: data, id, name }));
-        } else if (/(https?:\/\/.*\.(?:mp3|wav|ogg|aac|flac|opus))(?:\?.*)?$/i.test(data)) { // check if it's an audio url
-            check_dc(data);
-            let name = edit.make_unique_element_name("Audio");
-            edit.add_element(new audio_element(edit, { url: data, id, name, paused: true, transform: { width: 300, height: 50 } }));
-        } else if (/(https?:\/\/.*\.(?:mp4|webm|mkv|avi))(?:\?.*)?$/i.test(data)) { // check if it's an video url
-            check_dc(data);
-            let name = edit.make_unique_element_name("Video");
-            edit.add_element(new video_element(edit, { url: data, id, name, paused: true, transform: { width: 640, height: 360 } }));
-        } else {
-            let name = edit.make_unique_element_name("Text");
-            edit.add_element(new text_element(edit, { text: data, id, size: 32, font: "Arial", color: "#000000", name }));
-        }
-    });
+  read_data_from_clipboard().then((data) => {
+    let id = generate_element_id();
+    if (data.indexOf('data:image') === 0) {
+      let name = edit.make_unique_element_name('Image');
+      edit.add_element(new image_element(edit, { url: data, id, name }));
+    } else if (
+      /(https?:\/\/.*\.(?:jpg|jpeg|gif|png|bmp|webp|svg|ico|avif|jxl))(?:\?.*)?$/i.test(
+        data
+      )
+    ) {
+      // check if it's an image url
+      check_dc(data);
+      let name = edit.make_unique_element_name('Image');
+      edit.add_element(new image_element(edit, { url: data, id, name }));
+    } else if (
+      /(https?:\/\/.*\.(?:mp3|wav|ogg|aac|flac|opus))(?:\?.*)?$/i.test(data)
+    ) {
+      // check if it's an audio url
+      check_dc(data);
+      let name = edit.make_unique_element_name('Audio');
+      edit.add_element(
+        new audio_element(edit, {
+          url: data,
+          id,
+          name,
+          paused: true,
+          transform: { width: 300, height: 50 },
+        })
+      );
+    } else if (/(https?:\/\/.*\.(?:mp4|webm|mkv|avi))(?:\?.*)?$/i.test(data)) {
+      // check if it's an video url
+      check_dc(data);
+      let name = edit.make_unique_element_name('Video');
+      edit.add_element(
+        new video_element(edit, {
+          url: data,
+          id,
+          name,
+          paused: true,
+          transform: { width: 640, height: 360 },
+        })
+      );
+    } else {
+      let name = edit.make_unique_element_name('Text');
+      edit.add_element(
+        new text_element(edit, {
+          text: data,
+          id,
+          size: 32,
+          font: 'Arial',
+          color: '#000000',
+          name,
+        })
+      );
+    }
+  });
 }
 
 function keybind_paste_internal(edit, _e) {
-    if (edit.internal_clipboard) {
-        let new_element = edit.internal_clipboard.clone();
-        new_element.tf().x += 10;
-        new_element.tf().y += 10;
-        new_element.data.name = edit.make_unique_element_name(new_element.data.name);
-        edit.add_element(new_element);
-    }
+  if (edit.internal_clipboard) {
+    let new_element = edit.internal_clipboard.clone();
+    new_element.tf().x += 10;
+    new_element.tf().y += 10;
+    new_element.data.name = edit.make_unique_element_name(
+      new_element.data.name
+    );
+    edit.add_element(new_element);
+  }
 }
 
 function keybind_duplicate_selected_element(edit, _e) {
-    if (edit.selected_element) {
-        let new_element = edit.selected_element.clone();
-        new_element.tf().x += 10;
-        new_element.tf().y += 10;
-        new_element.data.name = edit.make_unique_element_name(new_element.data.name);
-        edit.add_element(new_element);
-    }
+  if (edit.selected_element) {
+    let new_element = edit.selected_element.clone();
+    new_element.tf().x += 10;
+    new_element.tf().y += 10;
+    new_element.data.name = edit.make_unique_element_name(
+      new_element.data.name
+    );
+    edit.add_element(new_element);
+  }
 }
 
 function keybind_delete_selected_element(edit, _e) {
-    if (edit.selected_element && edit.current_mode === EDIT_MODE.NONE) {
-        // remove from tickable elements
-        let index = edit.tickable_elements.indexOf(edit.elements.get(edit.selected_element.id()));
-        if (index > -1)
-            edit.tickable_elements.splice(index, 1);
+  if (edit.selected_element && edit.current_mode === EDIT_MODE.NONE) {
+    // remove from tickable elements
+    let index = edit.tickable_elements.indexOf(
+      edit.elements.get(edit.selected_element.id())
+    );
+    if (index > -1) edit.tickable_elements.splice(index, 1);
 
-        // remove html
-        edit.selected_element.html.remove();
-        edit.elements.delete(edit.selected_element.id());
-        send_command_delete_element(edit, edit.selected_element.id());
-        edit.select_element(null);
-        edit.rebuild_element_list();
-    }
+    // remove html
+    edit.selected_element.html.remove();
+    edit.elements.delete(edit.selected_element.id());
+    send_command_delete_element(edit, edit.selected_element.id());
+    edit.select_element(null);
+    edit.rebuild_element_list();
+  }
 }
 
 function keybind_toggle_overlay(edit, _e) {
-    edit.overlay.classList.toggle("hide-overlay");
+  edit.overlay.classList.toggle('hide-overlay');
 }
 
 function keybind_mirror_selected_element(edit, e) {
-    if (edit.selected_element) {
-        if (e.shiftKey)
-            edit.selected_element.flip_y();
-        else
-            edit.selected_element.flip_x();
+  if (edit.selected_element) {
+    if (e.shiftKey) edit.selected_element.flip_y();
+    else edit.selected_element.flip_x();
 
-        send_command_update_element(edit, edit.selected_element);
-    }
+    send_command_update_element(edit, edit.selected_element);
+  }
 }
 
 function keybind_enter_scale_mode(edit, e) {
-    if (edit.selected_element && edit.current_mode !== EDIT_MODE.SCALE) {
-        edit.enter_mode(EDIT_MODE.SCALE, MODE_AXIS.XY);
-        edit.initial_element_size = { x: edit.selected_element.tf().scale_x, y: edit.selected_element.tf().scale_y };
-    }
+  if (edit.selected_element && edit.current_mode !== EDIT_MODE.SCALE) {
+    edit.enter_mode(EDIT_MODE.SCALE, MODE_AXIS.XY);
+    edit.initial_element_size = {
+      x: edit.selected_element.tf().scale_x,
+      y: edit.selected_element.tf().scale_y,
+    };
+  }
 }
 
 function keybind_enter_move_mode(edit, _e) {
-    if (edit.selected_element && edit.current_mode !== EDIT_MODE.MOVE) {
-        edit.enter_mode(EDIT_MODE.MOVE, MODE_AXIS.XY);
-        edit.initial_element_size = { x: edit.selected_element.tf().x, y: edit.selected_element.tf().y };
-    }
+  if (edit.selected_element && edit.current_mode !== EDIT_MODE.MOVE) {
+    edit.enter_mode(EDIT_MODE.MOVE, MODE_AXIS.XY);
+    edit.initial_element_size = {
+      x: edit.selected_element.tf().x,
+      y: edit.selected_element.tf().y,
+    };
+  }
 }
 
 function keybind_enter_rotate_mode(edit, _e) {
-    if (edit.selected_element && edit.current_mode !== EDIT_MODE.ROTATE) {
-        edit.enter_mode(EDIT_MODE.ROTATE, MODE_AXIS.Z);
-        edit.initial_element_size = { rotation: edit.selected_element.tf().rotation_z };
-    }
+  if (edit.selected_element && edit.current_mode !== EDIT_MODE.ROTATE) {
+    edit.enter_mode(EDIT_MODE.ROTATE, MODE_AXIS.Z);
+    edit.initial_element_size = {
+      rotation: edit.selected_element.tf().rotation_z,
+    };
+  }
 }
 
 function keybind_exit_editor_modes(edit, _e) {
-    if (edit.selected_element) {
-        edit.leave_mode();
-    }
+  if (edit.selected_element) {
+    edit.leave_mode();
+  }
 }
 
 function keybind_switch_axis_x(edit, _e) {
-    if (edit.mode_axis !== MODE_AXIS.NONE) {
-        edit.mode_axis = MODE_AXIS.X;
-    }
+  if (edit.mode_axis !== MODE_AXIS.NONE) {
+    edit.mode_axis = MODE_AXIS.X;
+  }
 }
 
 function keybind_switch_axis_y(edit, _e) {
-    if (edit.mode_axis !== MODE_AXIS.NONE) {
-        edit.mode_axis = MODE_AXIS.Y;
-    }
+  if (edit.mode_axis !== MODE_AXIS.NONE) {
+    edit.mode_axis = MODE_AXIS.Y;
+  }
 }
 
 function keybind_switch_axis_z(edit, _e) {
-    if (edit.mode_axis !== MODE_AXIS.NONE) {
-        edit.mode_axis = MODE_AXIS.Z;
-    }
+  if (edit.mode_axis !== MODE_AXIS.NONE) {
+    edit.mode_axis = MODE_AXIS.Z;
+  }
 }
 
 function keybind_switch_axis_xy(edit, _e) {
-    if (edit.mode_axis !== MODE_AXIS.NONE) {
-        edit.mode_axis = MODE_AXIS.XY;
-    }
+  if (edit.mode_axis !== MODE_AXIS.NONE) {
+    edit.mode_axis = MODE_AXIS.XY;
+  }
 }
 
 function keybind_reset_transform(edit, _e) {
-    if (edit.selected_element) {
-        edit.selected_element.reset_transform();
-        edit.update_selected_element();
-        send_command_update_element(edit, edit.selected_element);
-        // block this event otherwise it'll reload the page
-        _e.preventDefault();
-    }
+  if (edit.selected_element) {
+    edit.selected_element.reset_transform();
+    edit.update_selected_element();
+    send_command_update_element(edit, edit.selected_element);
+    // block this event otherwise it'll reload the page
+    _e.preventDefault();
+  }
 }
 
 function keybind_favorite_selected_element(edit, _e) {
-    favorite_selected_element();
+  favorite_selected_element();
 }
 
 function keybind_toggle_selected_element_visibility(edit, _e) {
-    if (edit.selected_element) {
-        edit.selected_element.tf().visible = !edit.selected_element.tf().visible;
-        edit.selected_element.update();
-        edit.update_selected_element();
-        send_command_update_element(edit, edit.selected_element);
-    }
+  if (edit.selected_element) {
+    edit.selected_element.tf().visible = !edit.selected_element.tf().visible;
+    edit.selected_element.update();
+    edit.update_selected_element();
+    send_command_update_element(edit, edit.selected_element);
+  }
 }
 
 function keybind_reset_zoom(edit, _e) {
-    edit.editor_zoom = 1;
-    edit.editor_offset = [0, 0];
-    edit.update_zoom_and_offset();
+  edit.editor_zoom = 1;
+  edit.editor_offset = [0, 0];
+  edit.update_zoom_and_offset();
 }
 
 const editor_keybinds = [
-    { code: "KeyC", ctrl: true, function: keybind_copy },
-    { code: "KeyC", shift: true, function: keybind_copy },
-    { code: "KeyV", ctrl: true, function: keybind_paste },
-    { code: "KeyV", shift: true, function: keybind_paste_internal },
-    { code: "Delete", ctrl: false, function: keybind_delete_selected_element },
-    { code: "KeyX", ctrl: false, function: keybind_delete_selected_element },
-    { code: "KeyE", ctrl: true, function: keybind_toggle_overlay },
-    { code: "KeyM", ctrl: false, function: keybind_mirror_selected_element },
-    { code: "KeyM", shift: true, function: keybind_mirror_selected_element },
-    { code: "KeyS", ctrl: false, function: keybind_enter_scale_mode },
-    { code: "KeyG", ctrl: false, function: keybind_enter_move_mode },
-    { code: "KeyR", ctrl: false, function: keybind_enter_rotate_mode },
-    { code: "Escape", ctrl: false, function: keybind_exit_editor_modes },
-    { code: "KeyX", ctrl: false, function: keybind_switch_axis_x },
-    { code: "KeyY", ctrl: false, function: keybind_switch_axis_y },
-    { code: "KeyZ", ctrl: false, function: keybind_switch_axis_z },
-    { code: "KeyZ", shift: true, function: keybind_switch_axis_xy },
-    { code: "KeyR", ctrl: true, function: keybind_reset_transform },
-    { code: "KeyF", ctrl: false, function: keybind_favorite_selected_element },
-    { code: "KeyH", ctrl: false, function: keybind_toggle_selected_element_visibility },
-    { code: "KeyD", shift: true, function: keybind_duplicate_selected_element },
-    { code: "Space", ctrl: true, function: keybind_reset_zoom },
+  { code: 'KeyC', ctrl: true, function: keybind_copy },
+  { code: 'KeyC', shift: true, function: keybind_copy },
+  { code: 'KeyV', ctrl: true, function: keybind_paste },
+  { code: 'KeyV', shift: true, function: keybind_paste_internal },
+  { code: 'Delete', ctrl: false, function: keybind_delete_selected_element },
+  { code: 'KeyX', ctrl: false, function: keybind_delete_selected_element },
+  { code: 'KeyE', ctrl: true, function: keybind_toggle_overlay },
+  { code: 'KeyM', ctrl: false, function: keybind_mirror_selected_element },
+  { code: 'KeyM', shift: true, function: keybind_mirror_selected_element },
+  { code: 'KeyS', ctrl: false, function: keybind_enter_scale_mode },
+  { code: 'KeyG', ctrl: false, function: keybind_enter_move_mode },
+  { code: 'KeyR', ctrl: false, function: keybind_enter_rotate_mode },
+  { code: 'Escape', ctrl: false, function: keybind_exit_editor_modes },
+  { code: 'KeyX', ctrl: false, function: keybind_switch_axis_x },
+  { code: 'KeyY', ctrl: false, function: keybind_switch_axis_y },
+  { code: 'KeyZ', ctrl: false, function: keybind_switch_axis_z },
+  { code: 'KeyZ', shift: true, function: keybind_switch_axis_xy },
+  { code: 'KeyR', ctrl: true, function: keybind_reset_transform },
+  { code: 'KeyF', ctrl: false, function: keybind_favorite_selected_element },
+  {
+    code: 'KeyH',
+    ctrl: false,
+    function: keybind_toggle_selected_element_visibility,
+  },
+  { code: 'KeyD', shift: true, function: keybind_duplicate_selected_element },
+  { code: 'Space', ctrl: true, function: keybind_reset_zoom },
 ];
--- static/js/util.js
+++ static/js/util.js
@@ -15,183 +15,183 @@
    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-var IS_CHROME = navigator.userAgent.indexOf("Chrome") === -1 ? false : true;
+var IS_CHROME = navigator.userAgent.indexOf('Chrome') === -1 ? false : true;
 
 // enum for resize directions
 const EDIT_MODE = {
-    NONE: -1,
-    MOVE: 0,
-    SCALE: 1,
-    ROTATE: 2,
-    MOVE_CANVAS: 3,
+  NONE: -1,
+  MOVE: 0,
+  SCALE: 1,
+  ROTATE: 2,
+  MOVE_CANVAS: 3,
 };
 
 const MODE_AXIS = {
-    NONE: -1,
-    X: 0,
-    Y: 1,
-    Z: 2,
-    XY: 3,
+  NONE: -1,
+  X: 0,
+  Y: 1,
+  Z: 2,
+  XY: 3,
 };
 
 function get_cookie(name) {
-    const value = `; ${document.cookie}`;
-    const parts = value.split(`; ${name}=`);
-    if (parts.length === 2) return parts.pop().split(';').shift();
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(';').shift();
 }
 
 function set_cookie(name, value, days) {
-    let date = new Date();
-    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
-    document.cookie = `${name}=${value}; expires=${date.toUTCString()}; path=/`;
+  let date = new Date();
+  date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+  document.cookie = `${name}=${value}; expires=${date.toUTCString()}; path=/`;
 }
 
 function hex_to_rgba(hex) {
-    let bigint = parseInt(hex.substring(1), 16);
-    let r = (bigint >> 16) & 255;
-    let g = (bigint >> 8) & 255;
-    let b = bigint & 255;
-    return `rgba(${r}, ${g}, ${b}, 1)`;
+  let bigint = parseInt(hex.substring(1), 16);
+  let r = (bigint >> 16) & 255;
+  let g = (bigint >> 8) & 255;
+  let b = bigint & 255;
+  return `rgba(${r}, ${g}, ${b}, 1)`;
 }
 
 function $(query) {
-    var result = null;
-    if (typeof query === "object") {
-        result = query;
-    } else if (query[0] === "<") {
-        var temp = document.createElement("div");
-        temp.innerHTML = query;
-        result = temp.firstChild;
-    } else {
-        let qresult = document.querySelector(query);
-        if (qresult.length === undefined)
-            result = qresult;
-        else if (qresult[0] !== undefined)
-            result = qresult[0];
-        else
-            result = qresult;
-    }
+  var result = null;
+  if (typeof query === 'object') {
+    result = query;
+  } else if (query[0] === '<') {
+    var temp = document.createElement('div');
+    temp.innerHTML = query;
+    result = temp.firstChild;
+  } else {
+    let qresult = document.querySelector(query);
+    if (qresult.length === undefined) result = qresult;
+    else if (qresult[0] !== undefined) result = qresult[0];
+    else result = qresult;
+  }
 
-    if (result)
-        result.on = (id, handler) => result.addEventListener(id, (event) => handler(event));
+  if (result)
+    result.on = (id, handler) =>
+      result.addEventListener(id, (event) => handler(event));
 
-    if (result === document)
-        result.ready = (handler) => document.addEventListener("DOMContentLoaded", () => handler());
+  if (result === document)
+    result.ready = (handler) =>
+      document.addEventListener('DOMContentLoaded', () => handler());
 
-    return result;
+  return result;
 }
 
 function blob_to_base64(blob) {
-    return new Promise(resolve => {
-        let reader = new FileReader();
-        reader.onloadend = () => resolve(reader.result);
-        reader.readAsDataURL(blob);
-    });
+  return new Promise((resolve) => {
+    let reader = new FileReader();
+    reader.onloadend = () => resolve(reader.result);
+    reader.readAsDataURL(blob);
+  });
 }
 
 function blob_to_text(blob) {
-    return new Promise(resolve => {
-        let reader = new FileReader();
-        reader.onloadend = () => resolve(reader.result);
-        reader.readAsText(blob);
-    });
+  return new Promise((resolve) => {
+    let reader = new FileReader();
+    reader.onloadend = () => resolve(reader.result);
+    reader.readAsText(blob);
+  });
 }
 
 function parse_time_to_seconds(time) {
-    let seconds = 0;
-    let parts = time.split(":");
-    if (parts.length == 1) {
-        seconds = parseInt(parts[0]);
-    } else if (parts.length == 2) {
-        seconds = parseInt(parts[0]) * 60 + parseInt(parts[1]);
-    } else if (parts.length == 3) {
-        seconds = parseInt(parts[0]) * 3600 + parseInt(parts[1]) * 60 + parseInt(parts[2]);
-    }
-    return seconds;
+  let seconds = 0;
+  let parts = time.split(':');
+  if (parts.length == 1) {
+    seconds = parseInt(parts[0]);
+  } else if (parts.length == 2) {
+    seconds = parseInt(parts[0]) * 60 + parseInt(parts[1]);
+  } else if (parts.length == 3) {
+    seconds =
+      parseInt(parts[0]) * 3600 + parseInt(parts[1]) * 60 + parseInt(parts[2]);
+  }
+  return seconds;
 }
 
 function seconds_to_time(seconds, show_hours_and_miliseconds = true) {
-    let hours = Math.floor(seconds / 3600);
-    let minutes = Math.floor((seconds - hours * 3600) / 60);
-    seconds = seconds - hours * 3600 - minutes * 60;
-    // add leading zeros
-    hours = hours.toString().padStart(2, "0");
-    minutes = (minutes % 60).toString().padStart(2, "0");
+  let hours = Math.floor(seconds / 3600);
+  let minutes = Math.floor((seconds - hours * 3600) / 60);
+  seconds = seconds - hours * 3600 - minutes * 60;
+  // add leading zeros
+  hours = hours.toString().padStart(2, '0');
+  minutes = (minutes % 60).toString().padStart(2, '0');
 
-    let milliseconds = Math.floor((seconds - Math.floor(seconds)) * 1000);
-    milliseconds = milliseconds.toString().padStart(2, "0");
-    // limit milliseconds to 2 digits
-    milliseconds = milliseconds.substring(0, 2);
+  let milliseconds = Math.floor((seconds - Math.floor(seconds)) * 1000);
+  milliseconds = milliseconds.toString().padStart(2, '0');
+  // limit milliseconds to 2 digits
+  milliseconds = milliseconds.substring(0, 2);
 
-    seconds = (Math.floor(seconds) % 60).toString().padStart(2, "0");
+  seconds = (Math.floor(seconds) % 60).toString().padStart(2, '0');
 
-    if (show_hours_and_miliseconds)
-        return `${hours}:${minutes}:${seconds}.${milliseconds}`;
-    else if (hours === "00")
-        return `${minutes}:${seconds}`;
-    else
-        return `${hours}:${minutes}:${seconds}`;
+  if (show_hours_and_miliseconds)
+    return `${hours}:${minutes}:${seconds}.${milliseconds}`;
+  else if (hours === '00') return `${minutes}:${seconds}`;
+  else return `${hours}:${minutes}:${seconds}`;
 }
 
 function read_data_from_clipboard() {
-    return new Promise(resolve => resolve(navigator.clipboard.read()))
-        .then(data => {
-            for (let item of data) {
-                for (let type of item.types) {
-                    switch (type) {
-                        case "image/png":
-                        case "image/jpeg":
-                        case "image/gif":
-                        case "text/plain":
-                            return item.getType(type);
-                    }
-                }
-            }
-        }).then(data => {
-            if (data.type === "text/plain")
-                return blob_to_text(data);
-            else
-                return blob_to_base64(data);
-        });
+  return new Promise((resolve) => resolve(navigator.clipboard.read()))
+    .then((data) => {
+      for (let item of data) {
+        for (let type of item.types) {
+          switch (type) {
+            case 'image/png':
+            case 'image/jpeg':
+            case 'image/gif':
+            case 'text/plain':
+              return item.getType(type);
+          }
+        }
+      }
+    })
+    .then((data) => {
+      if (data.type === 'text/plain') return blob_to_text(data);
+      else return blob_to_base64(data);
+    });
 }
 
 function normalize(x, y) {
-    let length = Math.sqrt(x * x + y * y);
-    return [x / length, y / length];
+  let length = Math.sqrt(x * x + y * y);
+  return [x / length, y / length];
 }
 
 function vector_to_angle(x, y) {
-    [x, y] = normalize(x, y);
-    let angle = Math.acos(y);
-    if (x > 0)
-        angle = Math.PI + angle;
-    else
-        angle = Math.PI - angle;
-    return angle * 180 / Math.PI;
+  [x, y] = normalize(x, y);
+  let angle = Math.acos(y);
+  if (x > 0) angle = Math.PI + angle;
+  else angle = Math.PI - angle;
+  return (angle * 180) / Math.PI;
 }
 
 function lock_mouse(edit) {
-    edit.requestPointerLock = edit.requestPointerLock ||
-        edit.mozRequestPointerLock ||
-        edit.webkitRequestPointerLock;
+  edit.requestPointerLock =
+    edit.requestPointerLock ||
+    edit.mozRequestPointerLock ||
+    edit.webkitRequestPointerLock;
 
-    edit.requestPointerLock();
+  edit.requestPointerLock();
 }
 
 function unlock_mouse() {
-    document.exitPointerLock = document.exitPointerLock ||
-        document.mozExitPointerLock ||
-        document.webkitExitPointerLock;
+  document.exitPointerLock =
+    document.exitPointerLock ||
+    document.mozExitPointerLock ||
+    document.webkitExitPointerLock;
 
-    document.exitPointerLock();
+  document.exitPointerLock();
 }
 
 function clamp(value, min, max) {
-    return Math.min(Math.max(value, min), max);
+  return Math.min(Math.max(value, min), max);
 }
 
 function uuidv4() {
-    return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
-        (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
-    );
+  return '10000000-1000-4000-8000-100000000000'.replace(/[018]/g, (c) =>
+    (
+      c ^
+      (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
+    ).toString(16)
+  );
 }
